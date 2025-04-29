d76722d1125b325e6e3e88843a7fe840dc45ef22
ChristySchott <christyhauschild@gmail.com>
2025-04-29 18:09:11 -0300

    feat: validate login route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express'
 import asyncHandler from 'express-async-handler'
+import { celebrate, Segments } from 'celebrate'
 
 import { AuthController } from '../controllers/auth.controller.js'
+import { authLoginSchema } from '../models/user.model.js'
 
 export const authRoutes = Router()
 
-authRoutes.post('/auth/login', asyncHandler(AuthController.login))
+authRoutes.post(
+  '/auth/login',
+  celebrate({ [Segments.BODY]: authLoginSchema }),
+  asyncHandler(AuthController.login),
+)
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,27 +4,34 @@ import {
   getAuth as getFirebaseAuth,
   UserCredential,
 } from 'firebase/auth'
+import { FirebaseError } from 'firebase/app'
 
 import { User } from '../models/user.model.js'
 import { EmailAlreadyExistsError } from '../errors/email-already-exists.error.js'
+import { UnauthorizedError } from '../errors/unauthorized.error.js'
 
 export class AuthService {
   async create(user: User): Promise<UserRecord> {
-    try {
-      return await getAdminAuth().createUser({
+    return await getAdminAuth()
+      .createUser({
         email: user.email,
         password: user.password,
         displayName: user.name,
       })
-    } catch (err) {
-      if (err instanceof FirebaseAuthError && err.code === 'auth/email-already-exists') {
-        throw new EmailAlreadyExistsError()
-      }
-      throw err
-    }
+      .catch((err) => {
+        if (err instanceof FirebaseAuthError && err.code === 'auth/email-already-exists') {
+          throw new EmailAlreadyExistsError()
+        }
+        throw err
+      })
   }
 
-  login(email: string, password: string): Promise<UserCredential> {
-    return signInWithEmailAndPassword(getFirebaseAuth(), email, password)
+  async login(email: string, password: string): Promise<UserCredential> {
+    return await signInWithEmailAndPassword(getFirebaseAuth(), email, password).catch((err) => {
+      if (err instanceof FirebaseError && err.code === 'auth/invalid-credential') {
+        throw new UnauthorizedError()
+      }
+      throw err
+    })
   }
 }
