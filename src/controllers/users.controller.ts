ff7cf0589ee61eb3e7e3662b6e4c8ce594a899db
ChristySchott <christyhauschild@gmail.com>
2025-04-29 12:29:51 -0300

    feat: handle errors on users routes

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { getFirestore } from 'firebase-admin/firestore'
+import { ValidationError } from '../errors/validation.error.js'
 
 interface User {
   id: number | string
@@ -8,75 +9,84 @@ interface User {
 }
 
 export class UserController {
-  static async getAll(req: Request, res: Response) {
-    const snapshot = await getFirestore().collection('users').get()
-
-    if (snapshot.empty) {
-      res.send([])
-    }
-
-    const users = snapshot.docs.map(
-      (doc) =>
-        ({
-          id: doc.id,
-          ...doc.data(),
-        }) as User,
-    )
-
-    res.send(users)
-  }
-
-  static async getById(req: Request<{ id: string }>, res: Response) {
-    const userId = req.params.id
-
-    const doc = await getFirestore().collection('users').doc(userId).get()
-
-    if (!doc.exists) {
-      res.status(404).end()
-    }
-
-    const user = { id: doc.id, ...doc.data() } as User
-    res.send(user)
-  }
-
-  static async save(req: Request<Omit<User, 'id'>>, res: Response) {
-    const user = req.body
-
+  static async getAll(req: Request, res: Response, next: NextFunction) {
     try {
+      const snapshot = await getFirestore().collection('users').get()
+
+      const users = snapshot.docs.map(
+        (doc) =>
+          ({
+            id: doc.id,
+            ...doc.data(),
+          }) as User,
+      )
+      res.send(users)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async getById(req: Request<{ id: string }>, res: Response, next: NextFunction) {
+    try {
+      const userId = req.params.id
+      const doc = await getFirestore().collection('users').doc(userId).get()
+
+      const user = { id: doc.id, ...doc.data() } as User
+      res.send(user)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async save(req: Request<Omit<User, 'id'>>, res: Response, next: NextFunction) {
+    try {
+      const user = req.body
+
+      if (!user?.email) {
+        throw new ValidationError('E-mail is required.')
+      }
+
+      if (!user?.name) {
+        throw new ValidationError('Name is required.')
+      }
+
       await getFirestore().collection('users').add(user)
       res.status(201).end()
-    } catch {
-      res.status(500).end()
+    } catch (error) {
+      next(error)
     }
   }
 
-  static async update(req: Request<{ id: string }, null, Omit<User, 'id'>>, res: Response) {
-    const userId = req.params.id
-    const user = req.body as User
-
-    const userRef = getFirestore().collection('users').doc(userId)
-    const doc = await userRef.get()
-
-    if (!doc.exists) {
-      res.send({ status: 404, message: 'Usuário não encontrado' })
-    }
-
+  static async update(
+    req: Request<{ id: string }, null, Omit<User, 'id'>>,
+    res: Response,
+    next: NextFunction,
+  ) {
     try {
+      const userId = req.params.id
+      const user = req.body as User
+
+      const userRef = getFirestore().collection('users').doc(userId)
+
       await userRef.update({ name: user.name, email: user.email })
       res.status(204).end()
-    } catch {
-      res.status(500).end()
+    } catch (error) {
+      next(error)
     }
   }
 
-  static async delete(req: Request<{ id: string }, null, Omit<User, 'id'>>, res: Response) {
-    const userId = req.params.id
-
+  static async delete(
+    req: Request<{ id: string }, null, Omit<User, 'id'>>,
+    res: Response,
+    next: NextFunction,
+  ) {
     try {
+      const userId = req.params.id
+
       await getFirestore().collection('users').doc(userId).delete()
       res.status(204).end()
-    } catch {
-      res.status(500).end()
+    } catch (error) {
+      next(error)
     }
   }
 }
