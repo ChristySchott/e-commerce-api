4c9cf2ee2b87033e358dbf7003e608fd8b00d072
ChristySchott <christyhauschild@gmail.com>
2025-05-01 07:46:00 -0300

    feat: add routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express'
 
 import { authRoutes } from './auth.route.js'
-import { userRoutes } from './users.route.js'
+import { usersRoutes } from './users.route.js'
+import { companiesRoutes } from './companies.route.js'
 
 export const routes = (app: Express) => {
   app.use(express.json())
   app.use(authRoutes)
-  app.use(userRoutes)
+  app.use(usersRoutes)
+  app.use(companiesRoutes)
 }
